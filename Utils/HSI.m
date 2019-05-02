56762227513b641b743723f1464b9bae753fb88e
shinyypig <shinyypig@gmail.com>
2019-05-02 23:29:49 +0800

    add many new functions!

--- Utils/HSI.m
+++ Utils/HSI.m
@@ -80,6 +80,75 @@ classdef HSI < handle
             % This function will return a image: [m, n].
             im = reshape(y, obj.shape(1:2));
         end
+        
+        function remove_bands(obj, rm_list)
+            all_list = 1:obj.shape(end);
+            left_list = setdiff(all_list, rm_list);
+            obj.him = obj.him(:, :, left_list);
+        end
+        
+        function preprocess(obj, mode)
+            if ~exist('mode', 'var')
+                mode = 'norm';
+            end
+            
+            if strcmp(mode, 'norm')
+                m1 = max(obj.him, [], [1, 2]);
+                m2 = min(obj.him, [], [1, 2]);
+                obj.him = (obj.him - m2) ./ (m1 - m2);
+            elseif strcmp(mode, 'std')
+                mu = mean(obj.him, [1, 2]);
+                sigma = std(obj.him, [], [1, 2]);
+                obj.him = (obj.him - mu) ./ sigma;
+            else
+                error("Pararmeter 'mode' should be 'norm' or 'std'.")
+            end
+        end
+        
+        function Y = select_bands(obj, bands, mode)
+            Y = obj.him(:, :, bands);
+            if ~exist('mode', 'var')
+                mode = '2D';
+            end
+            
+            if strcmp(mode, '1D')
+                Y = reshape(Y, [], length(bands));
+            elseif strcmp(mode, '2D')
+                return;
+            else
+                error("Pararmeter 'mode' should be '1D' or '2D'.")
+            end
+        end
+        
+        function rgb = rgb(obj, degree)
+            if ~exist('degree', 'var')
+                degree = 0.5;
+            end
+            
+            [m, n, l] = size(obj.him);
+            
+            if degree <= 1/l || degree > 1
+                error('Parameter degree should between 1/bands_num and 1!');
+            end
+            
+            num = round(l * degree);
+
+            X = reshape(obj.him, [], l);
+            [Rn, Rs] = noise_signal_estim(obj.him);
+            Rn_ = pinv(sqrtm(Rn));
+            [V, ~] = eig(Rn_ * Rs * Rn_);
+            Y = X * V;
+
+            Y_ = Y(:, 1: num);
+            V_ = V(:, 1: num);
+            X_ = Y_ * V_'; 
+
+            idx = FVGBS(reshape(X_, [m, n, l]), [], 3);
+            rgb = obj.him(:, :, idx);
+            m1 = max(rgb, [], [1, 2]);
+            m2 = min(rgb, [], [1, 2]);
+            rgb = (rgb - m2) ./ (m1 - m2);
+        end
     end
 end
 
